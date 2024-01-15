d2232bb92e5b67f7dc319af901eb5e3c871fba6e
Maria Luisa Rios <marial.riosa@autonoma.edu.co>
2024-01-15 15:12:38 -0500

    inicio prueba 1

---
+++ prueba1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NAVI from './components/NAVI';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello world </h1>
+      <NAVI/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ prueba1/src/components/NAVI.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default  function NAVI(){
+  return (
+    <div>
+        <nav>
+            <a href="/html/">HTML</a> |
+            <a href="/css/">CSS</a> |
+            <a href="/js/">JavaScript</a> |
+            <a href="/python/">Python</a>
+        </nav>
+    </div>
+  )
+}
+
+
